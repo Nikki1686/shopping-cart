55a72b21e1bc4b6d7bfcb533313d963e5063731a
Nikki1686 <nikki.l.carthen@gmail.com>
2018-10-20 14:01:34 -0700

    start

--- js/app.js
+++ js/app.js
@@ -8,10 +8,13 @@ var Cart = function(items) {
 
 Cart.prototype.addItem = function(product, quantity) {
   // TODO: Fill in this instance method to create a new CartItem and add it to this.items
+  var newItem = new CartItem(product, quantity);
+  this.items.push(newItems);
 };
 
 Cart.prototype.saveToLocalStorage = function() {
   // TODO: Fill in this instance method to save the contents of the cart to localStorage
+  localStorage.setItem('cartItems', JSON.stringify(this.items));
 };
 
 Cart.prototype.removeItem = function(item) {
